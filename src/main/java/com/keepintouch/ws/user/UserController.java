ffe66cea7e861285ae6a933da13d78f65e08ea35
Salih Çetin <salihcetinteam@gmail.com>
2023-01-08 19:18:31 +0300

    UserService && GenericResponse Added

--- src/main/java/com/keepintouch/ws/user/UserController.java
+++ src/main/java/com/keepintouch/ws/user/UserController.java
@@ -1,7 +1,9 @@
 package com.keepintouch.ws.user;
 
+import com.keepintouch.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +13,13 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user) {
-        log.info(user.toString());
+    public GenericResponse createUser(@RequestBody User user) {
+        userService.save(user);
+
+        return new GenericResponse("user created");
     }
 }
