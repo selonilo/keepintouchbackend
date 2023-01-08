efa1df06dd453f3d07652ae3d6c5f7990340a333
Salih Çetin <salihcetinteam@gmail.com>
2023-01-08 19:05:19 +0300

    UserRepository created and UserEntity updated

--- src/main/java/com/keepintouch/ws/user/User.java
+++ src/main/java/com/keepintouch/ws/user/User.java
@@ -1,13 +1,28 @@
 package com.keepintouch.ws.user;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
     private String username;
 
     private String displayName;
 
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
