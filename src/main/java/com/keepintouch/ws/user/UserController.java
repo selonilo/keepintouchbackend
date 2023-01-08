7644d46aa2cd8d568450e6b61e9e49325134c7ed
Salih Çetin <salihcetinteam@gmail.com>
2023-01-08 21:59:02 +0300

    ApiError added

--- src/main/java/com/keepintouch/ws/user/UserController.java
+++ src/main/java/com/keepintouch/ws/user/UserController.java
@@ -1,12 +1,18 @@
 package com.keepintouch.ws.user;
 
+import com.keepintouch.ws.error.ApiError;
 import com.keepintouch.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -17,9 +23,16 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public GenericResponse createUser(@RequestBody User user) {
-        userService.save(user);
+    public ResponseEntity<?> createUser(@RequestBody User user) {
 
-        return new GenericResponse("user created");
+        if (user.getUsername() == null) {
+            ApiError error = new ApiError(400, "Validation error", "api/1.0/users");
+            Map<String,String> validationErrors = new HashMap<>();
+            validationErrors.put("username","username cannot be null");
+            error.setValidationErrors(validationErrors);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
+        userService.save(user);
+        return ResponseEntity.ok(new GenericResponse("user created"));
     }
 }
--- src/main/java/com/keepintouch/ws/user/UserService.java
+++ src/main/java/com/keepintouch/ws/user/UserService.java
@@ -1,16 +1,26 @@
 package com.keepintouch.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserService {
 
-    @Autowired
+
     private UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
     public void save(@RequestBody User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.saveAndFlush(user);
     }
 }
